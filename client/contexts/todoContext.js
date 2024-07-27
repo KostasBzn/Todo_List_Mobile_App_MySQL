871d9f49db4a5865991c4cb8ee27e4bda7882ded
Kostas Bouzianis <kostasbouzianis@gmail.com>
2024-07-27 09:49:44 +0200

    user and toso contexts created

---
+++ client/contexts/todoContext.js
@@ -0,0 +1,130 @@
+import { createContext, useState } from "react";
+import { BASE_URL } from "@env";
+import axios from "../config/axios.js";
+
+export const TodoContext = createContext();
+
+const TodoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([]);
+  const [todosForUser, setTodosForUser] = useState([]);
+  const [operationStatus, setOperationStatus] = useState({
+    success: false,
+    message: "",
+    type: "",
+  });
+  const [error, setError] = useState(null);
+
+  const baseUrl = BASE_URL;
+
+  // Create todo
+  const createTodo = async (userId, title, description) => {
+    const body = { userId, title, description };
+
+    try {
+      const response = await axios.post(`${baseUrl}/todos/new`, body);
+
+      if (response.data.success) {
+        setTodos([...todos, response.data.todo]);
+        setOperationStatus({
+          success: true,
+          message: "New todo created successfully",
+          type: "create",
+        });
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error creating new todo", error);
+    }
+  };
+
+  // Delete todo
+  const deleteTodo = async (todoId) => {
+    try {
+      const response = await axios.delete(`${baseUrl}/todos/delete/${todoId}`);
+
+      if (response.data.success) {
+        setTodos(todos.filter((todo) => todo._id !== todoId));
+        setOperationStatus({
+          success: true,
+          message: "Todo deleted successfully",
+          type: "delete",
+        });
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error deleting todo", error);
+    }
+  };
+
+  // Find todo
+  const findTodo = async (todoId) => {
+    try {
+      const response = await axios.get(`${baseUrl}/todos/find/${todoId}`);
+
+      if (response.data.success) {
+        return response.data.todo;
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error finding todo", error);
+    }
+  };
+
+  // Fetch all todos for a user
+  const fetchTodosForUser = async (userId) => {
+    try {
+      const response = await axios.get(`${baseUrl}/todos/all/${userId}`);
+
+      if (response.data.success) {
+        setTodosForUser(response.data.todosForUser);
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error fetching todos for user", error);
+    }
+  };
+
+  // Update todo
+  const updateTodo = async (todoId, title, description, status) => {
+    const body = { title, description, status };
+
+    try {
+      const response = await axios.put(`${baseUrl}/todos/edit/${todoId}`, body);
+
+      if (response.data.success) {
+        const updatedTodos = todos.map((todo) =>
+          todo._id === todoId ? response.data.updatedTodo : todo
+        );
+        setTodos(updatedTodos);
+        setOperationStatus({
+          success: true,
+          message: "Todo updated successfully",
+          type: "update",
+        });
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error updating todo", error);
+    }
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todos,
+        todosForUser,
+        operationStatus,
+        error,
+        createTodo,
+        deleteTodo,
+        findTodo,
+        fetchTodosForUser,
+        updateTodo,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoProvider;
---
+++ client/contexts/userContext.js
@@ -0,0 +1,97 @@
+import { createContext, useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { BASE_URL } from "@env";
+import axios from "../config/axios.js";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [operationStatus, setOperationStatus] = useState({
+    success: false,
+    message: "",
+    type: "",
+  });
+  const [error, setError] = useState(null);
+
+  const baseURL = BASE_URL;
+
+  // Sign in
+  const signIn = async (email, password) => {
+    const body = { email, password };
+
+    try {
+      const response = await axios.post(`${baseURL}/users/login`, body);
+
+      if (response.data.success) {
+        setUser(response.data.user);
+        setOperationStatus({
+          success: true,
+          message: "User signed in successfully",
+          type: "signIn",
+        });
+        await AsyncStorage.setItem("token", response.data.token);
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error signing in user", error);
+    }
+  };
+
+  // Sign up
+  const signUp = async (name, email, password) => {
+    const body = { name, email, password };
+
+    try {
+      const response = await axios.post(`${baseURL}/users/register`, body);
+      if (response.data.success) {
+        setOperationStatus({
+          success: true,
+          message: "User signed up successfully",
+          type: "signUp",
+        });
+      }
+    } catch (error) {
+      setError(error.message);
+      console.error("Error signing up user", error);
+    }
+  };
+
+  // Logged user
+  const loggedUser = async () => {
+    try {
+      const token = await AsyncStorage.getItem("token");
+      if (token) {
+        const response = await axios.get(`${baseURL}/users/logged`);
+
+        if (response.data.success) {
+          setUser(response.data.user);
+        }
+      }
+    } catch (error) {
+      console.error("Error logging user", error);
+      await AsyncStorage.removeItem("token");
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    loggedUser();
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        operationStatus,
+        error,
+        signIn,
+        signUp,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
